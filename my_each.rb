cef0daae062bd7db5f0e7b0da98d86c1bd05163d
hansenjl <jenn.leigh.hansen@gmail.com>
2020-10-22 09:00:39 -0700

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+
+def my_each(array) # put argument(s) here
+    # iterate over every element in an array 
+    counter = 0 
+    while counter < array.length #the counter is less than the length of the array 
+    # yield to the block for each individual element 
+      yield(array[counter])
+      counter += 1 
+    end
+    array # the return value 
 end
+
+
